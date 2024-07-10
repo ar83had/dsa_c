7c33a665319d9f3861a9b8e7a5a294b130bc56e3
ar83had <arshadshmim786@gmail.com>
2024-07-10 11:58:02 +0530

    binaryTreeSort {create avl tree  ,impore serch effiecey}

--- shorting/binaryTree_sort.c
+++ shorting/binaryTree_sort.c
@@ -6,25 +6,33 @@ typedef struct node
     int data;
     struct node* lchild;
     struct node* rchild;
+    int fac;
 }node;
 
-node* cre_bst(node*,int[]);
+node* cre_bst(node*,int,int);
 void asc(node*,int[]);
 void des(node*,int[]);
 node* des_bst(node*);
-
+node* cre_avl(node*,int,int*);
+node* l_rotate(node*);
+node* r_rotate(node*);
+void pre_order(node*);
 
 int main()
 {
     int arr[10]={0,1,2,3,4,5,6,7,8,9};
     node* root = NULL;
 
-    root=cre_bst(root,arr);
+    for(int c1=0;c1<10;c1++)
+        root=cre_bst(root,arr[c1],1);
 
     printf("\nassending order :");
     asc(root,arr);
     for(int c1=0;c1<10;c1++)
         printf(" %d",arr[c1]);
+
+    printf("\npreorder :");
+    pre_order(root);
 
     printf("\ndesending order :");
     des(root,arr);
@@ -36,44 +44,88 @@ int main()
     return 0;
 }
 
-node* cre_bst(node* root,int arr[])
+node* cre_bst(node* root,int el,int flag)
 {
-    node* ptr=NULL;
-    for(int c1=0;c1<10;c1++)
+    if(root==NULL)
     {
-        node* par=NULL;
-        root=ptr;
-        while(root!=NULL)
-        {
-            if(root->data > arr[c1])
-            {
-                par=root;
-                root=root->lchild;
-            }
-            else
-            {
-                par=root;
-                root=root->rchild;
-            }
-        }
-
         node* new_node = (node*)malloc(sizeof(node));
-        new_node->data=arr[c1];
-        new_node->lchild=NULL;
-        new_node->rchild=NULL;
+        new_node->lchild=NULL;new_node->rchild=NULL;new_node->fac=0;new_node->data=el;
+        return new_node;
+    }
 
-        if(par==NULL)
-            root=ptr=new_node;
+    if(root->data>el)
+    {
+        root->lchild=cre_bst(root->lchild,el,flag);
+        if(flag)
+            root = cre_avl(root,1,&flag);    
+
+    }
+    else
+    {
+        root->rchild=cre_bst(root->rchild,el,flag);
+        if(flag)
+            root = cre_avl(root,0,&flag);
+    }
+
+    return root;
+}
+
+node* cre_avl(node* root,int l,int* flag)
+{
+    if(l==1)
+    {
+        if(root->fac==0)
+            root->fac=1;
+        else if(root->fac==1)
+        {
+            root->fac=0;root->lchild->fac=0;
+            root = r_rotate(root);
+            *flag=0;
+        }
         else
         {
-            if(par->data>arr[c1])
-                par->lchild=new_node;
-            else
-                par->rchild=new_node;
+            root->fac=0;
+            *flag=0;
+        }
+    }
+    else
+    {
+        if(root->fac==0)
+            root->fac=-1;
+        else if(root->fac==1)
+        {
+            root->fac=0;
+            *flag=0;
+        }
+        else
+        {
+            root->fac=0;root->rchild->fac=0;
+            root = l_rotate(root);
+            *flag=0;
         }
     }
 
-    return ptr;
+    return root;
+}
+
+node* r_rotate(node* root)
+{
+    node* r_ptr = root->lchild;
+    node* temp=r_ptr->rchild;
+    r_ptr->rchild=root; 
+    root->lchild=temp;
+
+    return r_ptr;
+}
+
+node* l_rotate(node* root)
+{
+    node* r_ptr=root->rchild;
+    node* temp=r_ptr->lchild;
+    r_ptr->lchild=root;
+    root->rchild=temp;
+
+    return r_ptr;
 }
 
 void asc(node* root,int arr[])
@@ -111,4 +163,16 @@ node* des_bst(node* root)
     root->rchild=des_bst(root->rchild);
     free(root);
     return NULL;
+}
+
+void pre_order(node* root)
+{
+    if(root==NULL)
+        return;
+    
+    printf(" %d",root->data);
+    pre_order(root->lchild);
+    pre_order(root->rchild);
+
+    return;
 }
