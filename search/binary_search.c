40bb76d0c797538100739772d0cfbfc0c5c9709d
ar83had <arshadshmim786@gmail.com>
2024-07-13 10:54:24 +0530

    binaery_search.c{insert recursive search function}

--- search/binary_search.c
+++ search/binary_search.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 
 int binary_search(int[],int);
+int binary_search_rec(int[],int,int,int);
 
 int main()
 {
     int arr[10]={4,45,547,243,7,56,7,45,675,23};
 
     int n;
-    printf("\nenter search key :");
+    printf("\nenter search key :");~
     scanf("%d",&n);
 
-    int re =binary_search(arr,n);
+    //int re =binary_search(arr,n);
+    int re =binary_search_rec(arr,n,0,9);
 
     if(re==-1)
         printf("\n%d is founded !",n);
@@ -39,3 +41,20 @@ int binary_search(int arr[],int key)
 
     return -1;
 }
+
+int binary_search_rec(int arr[],int key,int low,int up)
+{
+    if(low>up)
+        return -1;
+    int mid = (low+up)/2;
+    if(arr[mid]==key)
+        return mid;
+    else if(arr[mid]>key)
+        low=mid+1;
+    else
+        up=mid-1;
+
+    int re = binary_search_rec(arr,key,low,up);
+
+    return re;
+}   
