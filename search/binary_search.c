5fe03994e5f3e37710c899d8dc0b117dab49fc5b
ar83had <arshadshmim786@gmail.com>
2024-07-13 10:35:30 +0530

    search/binary_search.c
    binary_search.c {complete}

---
+++ search/binary_search.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int binary_search(int[],int);
+
+int main()
+{
+    int arr[10]={4,45,547,243,7,56,7,45,675,23};
+
+    int n;
+    printf("\nenter search key :");
+    scanf("%d",&n);
+
+    int re =binary_search(arr,n);
+
+    if(re==-1)
+        printf("\n%d is founded !",n);
+    else
+        printf("%d is founded at %d position ",n,re);
+
+    return 0;
+}
+
+int binary_search(int arr[],int key)
+{
+    int low,up,mid;
+    low=0;up=9;
+
+    while(low<=up)      //low - up +1 = number of element in array;
+    {
+        mid=(low+up)/2;
+
+        if(arr[mid]==key)
+            return mid;
+        else if(arr[mid]>key)
+            low=mid+1;
+        else
+            up=mid-1;
+    }
+
+    return -1;
+}
