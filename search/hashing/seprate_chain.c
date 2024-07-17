c0b288df2f11d9eaf3c646559fce1dcaae244b58
ar83had <arshadshmim786@gmail.com>
2024-07-17 23:00:53 +0530

    seprate_chain.c{add search() & delete()}

--- search/hashing/seprate_chain.c
+++ search/hashing/seprate_chain.c
@@ -13,6 +13,10 @@ int n;node** hash_table;int m;
 int up_prime(int);
 node* insert_node(node*,node*);
 void display();
+void delete();
+node* del_node(node*,int);
+void search();
+int search_node(node*,int);
 
 int main()
 {
@@ -29,18 +33,35 @@ int main()
     do
     {
         int ch;
+        again1:
        printf("\n1)Insert\n2)Delete\n3)Display\n4)Search\n5)Exit\nEnter your choice :");
        scanf("%d",&ch);
+       if(ch>5 || ch<1)
+       {
+            printf("invalid choice !!!");
+            goto again1;
+       }
 
        switch(ch)
        {
             case 1:
                 insert();
                 break;
+            case 2:
+                delete();
+                break;
             case 3:
                 display();
                 break;
+            case 4:
+                search();
+                break;
+            case 5:
+                printf(".......EXIT........");
        }
+
+       if(ch==5)
+            break;
         printf("\nrepeate again :");
         scanf("\n");
         scanf("%[^\n]s",repeate);
@@ -93,6 +114,7 @@ node* insert_node(node* start,node* new_node)
     while(ptr->next!=NULL && ptr->next->data < new_node->data)
         ptr = ptr->next;
     
+    new_node->next=ptr->next;
     ptr->next=new_node;
     return start;
 }
@@ -113,4 +135,69 @@ void display()
     }
 
     return;
+}
+
+void delete()
+{
+    int key;
+    printf("\nenter delete key :");
+    scanf("%d",&key);
+
+    int add = key%m;
+    hash_table[add]=del_node(hash_table[add],key);
+    return;
+}
+
+node* del_node(node* start,int key)
+{
+    if(start==NULL)
+    {
+        printf("\nkey is not founded !");
+        return NULL;
+    }
+    else if(start->data==key)
+        return start->next;
+
+    node* ptr = start;
+    while( start->next != NULL && start->next->data != key)
+        start = start->next;
+    
+    if(start->next==NULL)
+        printf("\nkey is not founded !");
+    else
+        start->next=start->next->next;
+    
+    return ptr;
+}
+
+void search()
+{
+    int key;
+    printf("\nenter search key :");
+    scanf("%d",&key);
+
+    int add = key%m;
+    int fod = search_node(hash_table[add],key);
+
+    if(fod)
+        printf("\ndata founded !");
+    else
+        printf("\ndata is not founded !");
+
+    return;
+}
+
+int search_node(node* start,int key)
+{
+    if(start==NULL)
+        return 0;
+
+    while(start!=NULL)
+    {
+        if(start->data==key)
+            return 1;
+        start = start->next;
+    }    
+
+    return 0;
 }
