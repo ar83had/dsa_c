121424492ba50a4597ed0839abbc1e7104e25742
ar83had <arshadshmim786@gmail.com>
2024-07-27 23:28:56 +0530

    shorting/meadean {follow book reference}

--- shorting/meaden.c
+++ shorting/meaden.c
@@ -1,79 +1,48 @@
-//i think book method is best for quick sort;
-
 #include<stdio.h>
 
-int meaden_my(int[]);
-int meaden_book(int[]);
+int meadean(int[]);
+void swap(int[],int,int);
 
 int main()
 {
-    int arr[5];
+    int arr[10];
     printf("\nenter array element :");
-    for(int c1=0;c1<5;c1++)
+    for(int c1=0;c1<10;c1++)
     {
-        scanf("%d",&arr[c1]);
+        scanf(" %d",&arr[c1]);
     }
 
-    int re= meaden_my(arr);
-
-    printf("\n%d\n",meaden_book(arr));
-    for(int c1=0;c1<5;c1++)
+    printf("\narray element :");
+    for(int c1=0;c1<10;c1++)
         printf(" %d",arr[c1]);
 
-    return 0;
-
-}
-
-int meaden_my(int arr[])
-{
-    int lo=0;
-    int up=4;
-    int mid=2;
-
-    if(arr[lo]>arr[mid])
-        if(arr[mid]>arr[up])
-            printf("%d",up);
-        else if(arr[up]<arr[lo])
-            printf("%d",up);
-        else    
-            printf("%d",lo);
-    else
-        if(arr[lo]>arr[up])
-            printf("%d",lo);
-        else if(arr[mid]>arr[up])
-            printf("%d",up);
-        else
-            printf("%d",mid);
-        
+    int med = meadean(arr);
+    printf("\n%d",med);
     return 0;
 }
 
-int meaden_book(int arr[])
+int meadean(int arr[])
 {
-    int lo=0;int up=4;int mid=2;
-
-    if(arr[lo]>arr[mid])
-    {
-        int temp=arr[mid];
-        arr[mid]=arr[lo];
-        arr[lo]=temp;  
-    }
-    if(arr[lo]>arr[up])
-    {
-        int temp=arr[lo];
-        arr[lo]=arr[up];
-        arr[up]=temp;
-    }
-    if(arr[mid]>arr[mid+1])
-    {
-        int temp=arr[mid];
-        arr[mid]=arr[mid+1];
-        arr[mid+1]=temp;
-    }
-
-    int temp=arr[lo];
-    arr[lo]=arr[mid];
-    arr[mid]=temp;
+    int lo = arr[0];
+    int up = arr[9];
+    int mid = arr[(0+9)/2];
+    if(lo>mid)
+        swap(arr,0,4);
+    if(lo>up)
+        swap(arr,0,9);
+    if(mid>up)
+        swap(arr,4,9);
+    
+    swap(arr,0,4);
 
     return arr[0];
+}
+
+void swap(int arr[],int i,int j)
+{
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+
+    return;
 }
